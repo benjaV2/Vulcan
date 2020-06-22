3a78e56f2e645e9b296801800a22ac2ddf3cd87c
binyamin bendor <binyamin.bendor@bonditglobal.com>
2020-06-22 07:12:35 +0300

    Adding API Key

--- API_SERVER/rst_api/views.py
+++ API_SERVER/rst_api/views.py
@@ -8,15 +8,15 @@ import pymongo
 
 
 VULNER_URL = "https://vulners.com/api/v3"
+API_KEY = "9ZHMPEBGCOUKH389H29X41S6P99C9LRMVME1RQ7088IG0U6FOT3WDBM2VW7OND4T"
 
 # Create your views here.
 
 
 class plugin_fetcher(APIView):
     def get(self, request):
-        rs = requests.get(VULNER_URL + "/search/stats/")
-        rs.json()
-        response = requests.get(VULNER_URL + "/archive/collection/?type=nessus")
+        print("I'm IN!")
+        response = requests.get(VULNER_URL + "/archive/collection/?type=nessus", params={'apiKey': API_KEY})
         with zipfile.ZipFile(io.BytesIO(response.content)) as zip_files:
             for zip_file in zip_files.infolist():
                 rs = json.load(zip_files.open(zip_file))
